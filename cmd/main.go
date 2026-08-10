e2ba98f4b03052332a4b7640f1647cd71306b070
Abdel Qoddous Ferdous <k-aferdou@c1r1p5.hive.fi>
2026-08-10 11:48:11 +0300

    main in my branch

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlStmt := " CREATE TABLE IF NOT EXISTS users ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT    )"
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to SQLite!")
+}
