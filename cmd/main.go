e278df048618628b73b85f79d9a4f3262daa2f2d
Abdel Qoddous Ferdous <k-aferdou@c1r3p9.hive.fi>
2026-08-11 14:42:03 +0300

    db initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
+	"movies-api/internal/database"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "test.db")
+	db, err := database.NewDataBase("movieapi.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
+	if err := database.InitializeQuery(db); err != nil {
 		log.Fatal(err)
 	}
-
-	sqlStmt := " CREATE TABLE IF NOT EXISTS users ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,name TEXT    )"
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully connected to SQLite!")
 }
