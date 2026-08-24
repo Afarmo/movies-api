6feb2c49e1262a50d676a0f043af50ce7ca73ad4
Abderahman Afaryad <kallahti@hotmail.com>
2026-08-24 19:25:45 +0300

    Add genre & movie services

--- internal/service/genre.go
+++ internal/service/genre.go
@@ -1,1 +1,35 @@
 package service
+
+import (
+	"context"
+	"movies-api/internal/models"
+	"movies-api/internal/repository"
+)
+
+type GenreService struct {
+	repo repository.GenreRepo
+}
+
+func NewGenreService(repo repository.GenreRepo) *GenreService {
+	return &GenreService{repo: repo}
+}
+
+func (s *GenreService) InsertGenre(ctx context.Context, genre *models.Genre) error {
+	return s.repo.InsertGenre(ctx, genre)
+}
+
+func (s *GenreService) UpdateGenre(ctx context.Context, genre *models.Genre) error {
+	return s.repo.UpdateGenre(ctx, genre)
+}
+
+func (s *GenreService) DeleteGenre(ctx context.Context, id int64) error {
+	return s.repo.DeleteGenre(ctx, id)
+}
+
+func (s *GenreService) ListAllGenres(ctx context.Context) ([]*models.Genre, error) {
+	return s.repo.ListAllGenres(ctx)
+}
+
+func (s *GenreService) ListOneGenre(ctx context.Context, id int64) (*models.Genre, error) {
+	return s.repo.ListOneGenre(ctx, id)
+}
--- internal/service/movie.go
+++ internal/service/movie.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"context"
+	"movies-api/internal/models"
+	"movies-api/internal/repository"
+)
+
+type MovieService struct {
+	repo repository.MovieRepo
+}
+
+func NewMovieService(repo repository.MovieRepo) *MovieService {
+	return &MovieService{repo: repo}
+}
+
+func (s *MovieService) InsertMove(ctx context.Context, movie *models.Movie) error {
+	return s.repo.InsertMovie(ctx, movie)
+}
+
+func (s *MovieService) UpdateMovie(ctx context.Context, movie *models.Movie) error {
+	return s.repo.UpdateMovie(ctx, movie)
+}
+
+func (s *MovieService) DeleteMovie(ctx context.Context, id int64) error {
+	return s.repo.DeleteMovie(ctx, id)
+}
+func (s *MovieService) ListAllMovies(ctx context.Context) ([]*models.Movie, error) {
+	return s.repo.ListAllMovies(ctx)
+}
+func (s *MovieService) ListOneMovie(ctx context.Context, id int64) (*models.Movie, error) {
+	return s.repo.ListOneMovie(ctx, id)
+}
+
+func (s *MovieService) MovieByName(ctx context.Context, name string) (*models.Movie, error) {
+	return s.repo.MovieByName(ctx, name)
+}
