0cbed4098511d0800bd102dea280960f265a6e1e
Abderahman Afaryad <kallahti@hotmail.com>
2026-08-25 01:55:53 +0300

    Add GetOneGenreHandler (WIP)

--- internal/handlers/genre.go
+++ internal/handlers/genre.go
@@ -1,1 +1,18 @@
 package handlers
+
+import (
+	"movies-api/internal/service"
+	"net/http"
+)
+
+type GenreHandler struct {
+	genreService *service.GenreService
+}
+
+func NewGenreHandler(genreService *service.GenreService) *GenreHandler {
+	return &GenreHandler{genreService: genreService}
+}
+
+func (h *GenreHandler) GetOneGenreHandler(w http.ResponseWriter, req *http.Request) {
+
+}
