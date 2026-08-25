193dbacf205c6788a05b6850d7da351ca4bda336
Abderahman Afaryad <kallahti@hotmail.com>
2026-08-25 14:33:36 +0300

    Dependency injection and routing

--- internal/router/router.go
+++ internal/router/router.go
@@ -15,18 +15,22 @@ func NewRouter(actorHandler *handlers.ActorHandler,
 	mux.HandleFunc("GET /api/actor/{id}", actorHandler.GetOneActorHandler)
 	mux.HandleFunc("GET /api/actor", actorHandler.GetAllActorsHandler)
 	mux.HandleFunc("POST /api/actor", actorHandler.CreateActorHandler)
-	mux.HandleFunc("GET /api/actor/{name}", actorHandler.GetActorByNameHandler)
+	// mux.HandleFunc("GET /api/actor?name={name}", actorHandler.GetActorByNameHandler)
 	mux.HandleFunc("DELETE /api/actor/{id}", actorHandler.DeleteActor)
 	mux.HandleFunc("PATCH /api/actor/{id}", actorHandler.UpdateActor)
 
 	mux.HandleFunc("GET /api/genre/{id}", genreHandler.GetOneGenreHandler)
+	mux.HandleFunc("GET /api/genre", genreHandler.GetAllGenresHandler)
+	mux.HandleFunc("POST /api/genre", genreHandler.CreateGenreHandler)
+	mux.HandleFunc("DELETE /api/genre/{id}", genreHandler.DeleteGenreHandler)
+	mux.HandleFunc("PATCH /api/genre/{id}", genreHandler.UpdateGenreHandler)
 
 	mux.HandleFunc("GET /api/movie/{id}", movieHandler.GetOneMovieHandler)
 	mux.HandleFunc("GET /api/movie", movieHandler.GetAllMoviesHAndler)
 	mux.HandleFunc("POST /api/movie", movieHandler.CreateMovieHandler)
 	mux.HandleFunc("DELETE /api/movie/{id}", movieHandler.DeleteMovieHandler)
 	mux.HandleFunc("PATCH /api/movie/{id}", movieHandler.UpdateMovieHandler)
-	mux.HandleFunc("GET /api/movie/{name}", movieHandler.GetMovieByNameHandler)
+	// mux.HandleFunc("GET /api/movie/{name}", movieHandler.GetMovieByNameHandler)
 
 	return mux
 }
