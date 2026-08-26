4d2f436a2b3e5ab99e959b34eade246bee10fbc7
Abderahman Afaryad <kallahti@hotmail.com>
2026-08-26 23:56:16 +0300

    Refactor handlers, restructure and fix bugs

--- internal/handlers/actor.go
+++ internal/handlers/actor.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"database/sql"
 	"encoding/json"
 	"movies-api/internal/models"
 	"movies-api/internal/service"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 type ActorHandler struct {
@@ -19,124 +17,98 @@ func NewActorHandler(actorService *service.ActorService) *ActorHandler {
 }
 
 func (h *ActorHandler) GetOneActorHandler(w http.ResponseWriter, req *http.Request) {
-
-	ctx := req.Context()
-
-	idString := req.PathValue("id")
-	id, err := strconv.ParseInt(idString, 10, 64)
+	actorID, err := strconv.ParseInt(req.PathValue("id"), 10, 64)
 	if err != nil {
 		http.Error(w, "invalid actor id", http.StatusBadRequest)
 		return
 	}
 
-	actor, err := h.actorService.ListOneActor(ctx, id)
+	actor, err := h.actorService.ListOneActor(req.Context(), actorID)
 	if err != nil {
-		if strings.Contains(err.Error(), sql.ErrNoRows.Error()) {
-			http.Error(w, "actor not found", http.StatusNotFound)
-			return
-		}
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actor)
 }
 
 func (h *ActorHandler) GetAllActorsHandler(w http.ResponseWriter, req *http.Request) {
-
-	ctx := req.Context()
-	actors, err := h.actorService.ListAllActors(ctx)
+	actors, err := h.actorService.ListAllActors(req.Context())
 	if err != nil {
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actors)
-
 }
 
 func (h *ActorHandler) CreateActorHandler(w http.ResponseWriter, req *http.Request) {
-	actor := models.Actor{}
-	err := json.NewDecoder(req.Body).Decode(&actor)
-	if err != nil {
+	var actor models.Actor
+
+	if err := json.NewDecoder(req.Body).Decode(&actor); err != nil {
 		http.Error(w, "invalid json", http.StatusBadRequest)
 		return
 	}
-	ctx := req.Context()
-	err = h.actorService.InsertActor(ctx, &actor)
-	if err != nil {
-		http.Error(w, "could not create actor", http.StatusInternalServerError)
+
+	if err := h.actorService.InsertActor(req.Context(), &actor); err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(actor)
 }
 
 func (h *ActorHandler) GetActorByNameHandler(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-	name := req.PathValue("name")
+	name := req.URL.Query().Get("name")
 
-	actors, err := h.actorService.ActorsByName(ctx, name)
+	actors, err := h.actorService.ActorsByName(req.Context(), name)
 	if err != nil {
-		if strings.Contains(err.Error(), sql.ErrNoRows.Error()) {
-			http.Error(w, "No Actor Found With the name : "+name, http.StatusNotFound)
-			return
-		}
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actors)
 }
 
 func (h *ActorHandler) DeleteActor(w http.ResponseWriter, req *http.Request) {
-	idString := req.PathValue("id")
-	ctx := req.Context()
+	actorID, err := strconv.ParseInt(req.PathValue("id"), 10, 64)
+	if err != nil {
+		http.Error(w, "invalid actor id", http.StatusBadRequest)
+		return
+	}
 
-	id, err := strconv.ParseInt(idString, 10, 64)
-	if err != nil {
-		http.Error(w, "Invalid id", http.StatusBadRequest)
+	if err := h.actorService.DeleteActor(req.Context(), actorID); err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
-	err = h.actorService.DeleteActor(ctx, id)
-	if err != nil {
-		if strings.Contains(err.Error(), sql.ErrNoRows.Error()) {
-			http.Error(w, "NO such Actor", http.StatusNotFound)
-			return
-		}
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusAccepted)
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (h *ActorHandler) UpdateActor(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-	idString := req.PathValue("id")
-
-	id, err := strconv.ParseInt(idString, 10, 24)
+	actorID, err := strconv.ParseInt(req.PathValue("id"), 10, 64)
 	if err != nil {
-		http.Error(w, "Invalid id", http.StatusBadRequest)
+		http.Error(w, "invalid actor id", http.StatusBadRequest)
 		return
 	}
-	actor := models.Actor{}
-	actor.ID = int(id)
-	err = json.NewDecoder(req.Body).Decode(&actor)
-	if err != nil {
+
+	var actor models.Actor
+
+	if err := json.NewDecoder(req.Body).Decode(&actor); err != nil {
 		http.Error(w, "invalid json", http.StatusBadRequest)
 		return
 	}
-	err = h.actorService.UpdateActor(ctx, &actor)
-	if err != nil {
-		if strings.Contains(err.Error(), sql.ErrNoRows.Error()) {
-			http.Error(w, "NO such Actor", http.StatusNotFound)
-			return
-		}
+	actor.ID = int(actorID)
 
-		http.Error(w, "Internal Server Error ", http.StatusInternalServerError)
+	if err := h.actorService.UpdateActor(req.Context(), &actor); err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusAccepted)
 
+	w.WriteHeader(http.StatusOK)
 }
